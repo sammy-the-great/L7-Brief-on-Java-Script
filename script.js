7911b282e28ebcc8f434118a8b2420b17758dcb8
sammy-the-great <rmg42016@gmail.com>
2024-12-28 18:14:33 +0100

    Brief on Java Script

---
+++ script.js
@@ -0,0 +1,22 @@
+var x=-18.999
+var y="sammy gold"
+var b=null
+var c=true
+document.write(typeof(b))
+document.write("<br>")
+var age=10
+
+if(age>=18){
+    document.write("you are old enough")
+}
+ else{
+    document.write("you are young")
+ }
+ {
+    document.write("<br>")
+ }
+ let i=1
+ while(i<=10){
+    document.write(i+"<br>")
+    i++
+ }
